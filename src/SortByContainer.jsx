408c5d3b9b044e96dc7f9c823a44b160605ffde9
Lauren <laurenfelty01@gmail.com>
2024-06-13 15:04:09 -0500

    conditionally render out of stock pop up

--- src/SortByContainer.jsx
+++ src/SortByContainer.jsx
@@ -21,8 +21,14 @@ function SortByContainer() {
   //Initailly it is set to null because there is no data
   //The setter funciton will be updated once the data is fetched from the server
   const [itemData, setItemData] = useState(null);
+
+  //Handle state for conditionally rendering Out of Stock pop up
+  const [buttonClicked, setButtonClicked] = useState(null);
+
   //Declare a function to handle the onClick of the Out of Stock button
   const handleClick = async () => {
+    //set the button clicked to be true
+    setButtonClicked(true);
     try {
       const response = await fetch(`http://localhost:8080/outOfStock`);
       if (response.ok) {
@@ -40,6 +46,7 @@ function SortByContainer() {
 
   //When the popup is closed the setItemData should be reset to null
   const handleClose = () => {
+    setButtonClicked(null);
     setItemData(null);
   };
 
@@ -56,6 +63,35 @@ function SortByContainer() {
           </div>
         </div>
       </nav>
+
+      {/* Conditionally render the popup by looking to see if the selectedType state is truth aka somehting was selected*/}
+      {buttonClicked && (
+        <>
+          <div className='overlay' onClick={handleClose}></div>
+          <div className='popupContainer'>
+            <div className='popup-content'>
+              <span className='close' onClick={handleClose}>
+                Exit
+              </span>
+              <h2>Out of Stock Items</h2>
+              <div className='grid-header'>Items</div>
+              {itemData && Array.isArray(itemData.items) && (
+                <div id='outOfStockItems'>
+                  {itemData.items.map((category, index) => (
+                    <React.Fragment key={index}>
+                      {category.items.map((item, itemIndex) => (
+                        <React.Fragment key={itemIndex}>
+                          <div className='grid-item-outOfStock'>{item.item}</div>
+                        </React.Fragment>
+                      ))}
+                    </React.Fragment>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
